b038a205f1141092aa57e596b90a4195589f9da9
junran <kingsleyl0107@gmail.com>
2023-10-25 16:57:39 +0800

    improve output format

--- main.py
+++ main.py
@@ -2,12 +2,49 @@ from utils.reader import image_to_pil
 from utils.transform import img_to_tensor
 from NetWork.model import predict
 from reference.index_formula import cal_normal_index, cal_AI, cal_EPI
+import os
+from tqdm import tqdm
 
-path = r'./images/8pcguVQpxRMikWptAhcM8w.jpg'
-pil_image = image_to_pil(path)
-singleton_batch, output_size = img_to_tensor(pil_image)
-result = predict(singleton_batch, output_size)
-print(f"GreenView Index:{cal_normal_index(result, 'GVI')}")
-print(f"SpatialBoundary Index:{cal_normal_index(result, 'SBI')}")
-print(f"Accessibility Index:{cal_AI(result)}")
-print(f"EnclosurePerception Index:{cal_EPI(result)}")
+
+class IndexCalculator:
+    def __init__(self) -> None:
+        self.indics_values = {
+            "SBI": [],
+            "SLI": [],
+            "SVI": [],
+            "TFI": [],
+            "PSI": [],
+            "SI": [],
+            "GVI": [],
+            "SAI": [],
+            "AI": [],
+            "EPI": []
+        }
+
+    def gen_idx(self, pano_id):
+        path = rf'C:\Users\20191\Desktop\streetview_images_crawler_Google\2022_GSV\{pano_id}.jpg'
+        pil_image = image_to_pil(path)
+        singleton_batch, output_size = img_to_tensor(pil_image)
+        result = predict(singleton_batch, output_size)
+        for idx in ['SBI', 'SLI', 'SVI', 'TFI', 'PSI', 'SI', 'GVI', 'SAI']:
+            self.indics_values[idx].append(cal_normal_index(result, idx))
+        self.indics_values['AI'].append(cal_AI(result))
+        self.indics_values['EPI'].append(cal_EPI(result))
+
+    def release_all(self):
+        import pandas as pd
+        result = pd.DataFrame(self.indics_values)
+        return result
+
+
+if __name__ == '__main__':
+    calculator = IndexCalculator()
+    for image in tqdm(
+            os.listdir(
+                r'C:\Users\20191\Desktop\streetview_images_crawler_Google\2022_GSV'
+            )):
+        if image.endswith('.jpg'):
+            panoid = os.path.splitext(image)[0]
+            calculator.gen_idx(panoid)
+    res = calculator.release_all()
+    print(res)
--- reference/index_formula.py
+++ reference/index_formula.py
@@ -29,13 +29,19 @@ def cal_EPI(pred):
     # 围合感知指数 enclosure perception index
     EPI_Numerator_pixels = cal_pixels(pred, 'EPI')
     EPI_Denominator_pixels = cal_pixels(pred, "EPI_D")
-
-    return round(EPI_Numerator_pixels / EPI_Denominator_pixels, 2)
+    try:
+        res = round(EPI_Numerator_pixels / EPI_Denominator_pixels, 2)
+    except Exception:
+        res = 999
+    return res
 
 
 def cal_AI(pred):
     # 通行性指数 accessibility index
     AI_Numerator_pixels = cal_pixels(pred, 'AI')
     AI_Denominator_pixels = cal_pixels(pred, "AI_D")
-
-    return round(AI_Numerator_pixels / AI_Denominator_pixels, 2)
+    try:
+        res = round(AI_Numerator_pixels / AI_Denominator_pixels, 2)
+    except Exception:
+        res = 999
+    return res
